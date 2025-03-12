e2824edde2d7b48cbcce0c3e2525715e08d519ff
Ivan <chaca360@gmail.com>
2025-03-12 17:27:18 -0500

    fix: api

---
+++ src/app/about/page.jsx
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+        <div className="text-center ">
+            <h1>about</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio hic saepe voluptate veniam tenetur ab reprehenderit, et ullam voluptatibus ratione ad numquam quis veritatis error eum vitae voluptates repellat praesentium!</p>
+        </div>
+    );
+}
